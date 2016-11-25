3ef13bb1ff00e8a558125c2f2b9f33871e960454
aifos710 <sofia.m.caballero@gmail.com>
2016-11-25 14:45:57 -0500

    adding dragon

--- js/app.js
+++ js/app.js
@@ -2,3 +2,12 @@ var  cx = $('.esfera');
 cx.click(function(){
   $(this).toggleClass('brillo');
 });
+
+/*$(function() {
+  var count = 0;
+  $(".esfera-estrella").click(function() {
+    if (count++ == 7) {
+      $(this).addClass(".dragon");
+    }
+  })
+});*/
